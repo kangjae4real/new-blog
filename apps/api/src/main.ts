c1ba6ed3f262d2077fda71cf8d959ca9b62231e8
Kangjae Choi <awpe89@gmail.com>
2024-04-15 15:47:32 +0900

    Add all applications dockerize codes

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@/app.module';
 
 async function app() {
   const app = await NestFactory.create(AppModule);
