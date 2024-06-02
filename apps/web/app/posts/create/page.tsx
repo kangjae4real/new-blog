688ed467210c0f06f00c73e99d67221045dad0d8
Kangjae Choi <awpe89@gmail.com>
2024-06-02 17:00:25 +0900

    feat(web): add posts/create page mock

---
+++ apps/web/app/posts/create/page.tsx
@@ -0,0 +1,9 @@
+import PageLayout from "@/components/layouts/page-layout";
+
+export default function PostCreatePage() {
+  return (
+    <PageLayout>
+      <span>create</span>
+    </PageLayout>
+  );
+}
